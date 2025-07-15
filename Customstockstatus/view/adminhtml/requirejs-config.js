1f849c9fdf49657e0cef3eb5cd1d000289e45f70
Santi Moltó <52293855+santimolto@users.noreply.github.com>
2025-07-15 17:49:53 +0200

    Delete Customstockstatus directory

--- Customstockstatus/view/adminhtml/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'mage/backend/menu': {
-                'Santi_Customstockstatus/js/menu-icon': true
-            }
-        }
-    }
-};
